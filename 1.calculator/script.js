9635d4e494fe9c8c80b881848b0db162d3d65b5e
mahesh-tirmali <maheshtirmali2@gmail.com>
2023-01-14 16:18:57 +0530

    added try catch functionality to handle the errors for wrong calculation format

--- 1.calculator/script.js
+++ 1.calculator/script.js
@@ -3,14 +3,21 @@ let buttons = document.querySelectorAll('.button');
 
 Array.from(buttons).forEach((button) =>{
     button.addEventListener('click',(e) => {
-        console.log(e.target.innerText);
-        if (e.target.innerText == "=") {
-            string = eval(string);
-        }else if (e.target.innerText.toLowerCase() == "c") {
+        //console.log(e.target.innerText);
+        try {
+            if (e.target.innerText == "=") {
+                string = eval(string);
+            }else if (e.target.innerText.toLowerCase() == "c") {
+                string = "";
+            }else{
+                string += e.target.innerText;
+            }        
+            document.querySelector('input').value = string;
+          }
+          catch(err) {
+            alert("Please select correct calculation format..");
             string = "";
-        }else{
-            string += e.target.innerText;
-        }        
-        document.querySelector('input').value = string;
+            document.querySelector('input').value = string;
+          }        
     })
 })
