e785eaa9d395bd1e2a4d16e6d5849ffa89cd748a
Mahesh Tirmali <maheshtirmali2@gmail.com>
2023-09-23 17:35:45 +0530

    Implement logic to generate random color code and show color code and background with that color on screen and copy that code automatically to clipboard

---
+++ 3.Random_color_generator/script.js
@@ -0,0 +1,15 @@
+//Generate random color
+const getColor = () => {
+  const randomNumber = Math.floor(Math.random() * 16777215);
+  const randomCode = '#' + randomNumber.toString(16);
+  console.log(randomNumber, randomCode);
+  document.body.style.backgroundColor = randomCode;
+  document.getElementById('color-code').innerText = randomCode;
+  navigator.clipboard.writeText(randomCode);
+};
+
+//event call
+document.getElementById('btn').addEventListener('click', getColor);
+
+//initial call
+getColor();
